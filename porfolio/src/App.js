59ab9d36267a0311bc70892c5a62b4376feba24d
Miguel Alcantar <malcantar185@gmail.com>
2021-05-26 20:53:46 -0700

    links

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
+import './components/App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar/index"
-// import {BrowserRouter as Router, Route } from "react-router";
+
 
 function App () {
     return (
-        // <Router>
-            <div>
-                <Navbar />
-                {/* <Header />
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/porfolio" component={Porfolio} />
-                <Route exact path="/contact" component={Contact} />
-                <Footer /> */}
-            </div>
-        // </Router>
+       <Navbar />
     );
 }
 
--- porfolio/src/components/Navbar/index.js
+++ porfolio/src/components/Navbar/index.js
@@ -2,19 +2,25 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Miguel Alcantar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" href="#">About Me</a>
+        </li> 
+        <li className="nav-item">
+          <a className="nav-link" href="#">Porfolio</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Resume</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
         </li>
       
       </ul>
