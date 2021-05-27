d70e86ef2d82bed98b5ea11216dbd68f530150c0
Miguel Alcantar <malcantar185@gmail.com>
2021-05-27 00:24:41 -0700

    adding

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './components/App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar/index"
+
+
+function App () {
+    return (
+       <Navbar />
+    );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Miguel Alcantar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Me</a>
+        </li> 
+        <li className="nav-item">
+          <a className="nav-link" href="#">Porfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Resume</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      
+      </ul>
+    
+    </div>
+  </div>
+</nav>
+    )
+}
+
+export default Navbar;
